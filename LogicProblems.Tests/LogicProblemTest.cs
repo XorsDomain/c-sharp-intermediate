58af0836993cea54e1f0bb4484cf2677d1313395
George Marshall <gmarshall@catalyte.io>
2022-01-28 17:52:06 -0500

    Created methods and unit tests

--- LogicProblems.Tests/LogicProblemTest.cs
+++ LogicProblems.Tests/LogicProblemTest.cs
@@ -1,9 +1,108 @@
 using System;
+using Xunit;
+using System.Linq;
+using System.Collections.Generic;
+using LogicProblems;
 
 namespace LogicProblems.Tests
 {
-    public class LogicProblemTest
+    public class LogicProblems
     {
         //TODO: Implement all requirements as specified in the requirements document
+        LogicProblem logicProblem;
+
+        public LogicProblems()
+        {
+            logicProblem = new LogicProblem();
+        }
+
+        [Fact]
+        public void GetAverage_ReturnDouble_NonEmpty()
+        {
+            int[] numbers = { 4, 8, 13 };
+            double expected = 8.33;
+            var actual = logicProblem.Average(numbers);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetAverage_ReturnsZero_Empty()
+        {
+            int[] numbers = { };
+            double expected = 0.00;
+            var actual = logicProblem.Average(numbers);
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void GetAverage_SingleInt_ReturnsDouble()
+        {
+            int[] numbers = { 5 };
+            double expected = 5.00;
+            var actual = logicProblem.Average(numbers);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetAverage_AtLeastOneNegative_ReturnsError()
+        {
+            int[] numbers = { 4, 8, -27 };
+            string expected = "scores must be positive";
+            var errorMsg =  Assert.Throws<NotImplementedException>(() => logicProblem.Average(numbers));
+            Assert.Equal(expected, errorMsg.Message);
+        }
+
+        [Fact]
+        public void DistinctLadder_ReturnsPaths_SmallNumber()
+        {
+            int number = 5;
+            int expected = 8;
+            var actual = logicProblem.DistinctLadderPaths(number);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void DistinctLadder_ReturnPaths_AnotherNumber()
+        {
+            int number = 10;
+            int expected = 89;
+            var actual = logicProblem.DistinctLadderPaths(number);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void DistinctLadder_ReturnPaths_Zero()
+        {
+            int number = 0;
+            int expected = 0;
+            var actual = logicProblem.DistinctLadderPaths(number);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void CheckLastWord_InString_ReturnsLength()
+        {
+            string str = "Hello World";
+            int expected = 5;
+            var actual = logicProblem.LastWordLength(str);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void CheckLastWord_WhitespaceString_ReturnsZero()
+        {
+            string str = " ";
+            int expected = 0;
+            var actual = logicProblem.LastWordLength(str);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void CheckLastWord_EmptyString_ReturnsError()
+        {
+            string str = "";
+            string expected = "input must not be an empty string";
+            var errorMsg = Assert.Throws<NotImplementedException>(() => logicProblem.LastWordLength(str));
+            Assert.Equal(expected, errorMsg.Message);
+        }
     }
 }
--- LogicProblems/LogicProblem.cs
+++ LogicProblems/LogicProblem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace LogicProblems
 {
@@ -8,15 +9,44 @@ namespace LogicProblems
         //TODO: Implement all requirements as defined in the project description.
         public double Average(int[] scores)
         {
-            throw new NotImplementedException();
+            int i = 0;
+            int sum = 0;
+            double average = 0.0;
+            if (scores.Length == 0)
+            {
+                return 0.00;
+            }
+            for (i = 0; i < scores.Length; i++)
+            {
+                sum += scores[i];
+                if (scores[i] < 0)
+                {
+                    throw new NotImplementedException("scores must be positive");
+                }
+            }
+            average = (double)sum / scores.Length;
+            double total = Math.Round(average, 2, MidpointRounding.AwayFromZero);
+            return total;
         }
 
         public decimal DistinctLadderPaths(int rungs)
         {
-            throw new NotImplementedException();
+            if (rungs < 0)
+            {
+                throw new NotImplementedException("ladders can't have negative rungs");
+            }
+            if (rungs <= 3)
+            {
+                return rungs;
+            }
+            // if (rungs == 2) 
+            // {
+            //     return rungs;
+            // }
+            return DistinctLadderPaths(rungs - 1) + DistinctLadderPaths(rungs - 2);
         }
 
-        
+
         public List<List<string>> GroupStrings(string[] strs)
         {
             throw new NotImplementedException();
@@ -24,7 +54,14 @@ namespace LogicProblems
 
         public int LastWordLength(string text)
         {
-            throw new NotImplementedException();
+            List<string> newText = text.Trim().Split(' ').ToList();
+            string last = newText.Last();
+            int lastLength = last.Length;
+            if (String.IsNullOrEmpty(text))
+            {
+                throw new NotImplementedException("input must not be an empty string");
+            }
+            return lastLength;
         }
     }
 }
