48d3637b51ddeb68e4e9658a832efeedbd8fb558
George Marshall <gmarshall@catalyte.io>
2022-02-01 13:43:36 -0500

    Edited various files

--- LogicProblems/ILogicProblem.cs
+++ LogicProblems/ILogicProblem.cs
@@ -3,26 +3,46 @@ using System.Collections.Generic;
 
 namespace LogicProblems
 {
- /*
- ***********************************************************
- ***********************************************************
- ***********************************************************
- **************DO NOT CHANGE THIS INTERFACE!!!**************
- ***********************************************************
- ******** EXCEPTION: TO ADD XML STRUCTURED COMMENTS ********
- ******************TO THE PUBLIC METHODS********************
- ***********************************************************
- ***********************************************************
- */
+    /*
+    ***********************************************************
+    ***********************************************************
+    ***********************************************************
+    **************DO NOT CHANGE THIS INTERFACE!!!**************
+    ***********************************************************
+    ******** EXCEPTION: TO ADD XML STRUCTURED COMMENTS ********
+    ******************TO THE PUBLIC METHODS********************
+    ***********************************************************
+    ***********************************************************
+    */
 
     public interface ILogicProblem
     {
+        /// <summary>
+        /// This method takes an array of integers, named "scores", and returns a double using the "Average" operation.
+        /// </summary>
+        /// <param name="scores">scores is the array of integers.</param>
+        /// <returns>Method will return the total Average of the array into a double.</returns>
         double Average(int[] scores);
 
+        /// <summary>
+        /// This method takes a given string, named "text", and returns the last word's length as an integer.
+        /// </summary>
+        /// <param name="text">text is the given string, such as "Hello World".</param>
+        /// <returns>This method will return lastLength, which is the integer of the last word's length. Ex: "Hello World" -> "World" -> 5.</returns>
         int LastWordLength(string text);
 
+        /// <summary>
+        /// This method takes an integer, named "rungs", and returns another integer displaying the number of times you can reach the given integer.
+        /// </summary>
+        /// <param name="rungs">rungs is the integer given to determine its paths.</param>
+        /// <returns>econdNumber is returned</returns>
         decimal DistinctLadderPaths(int rungs);
 
+        /// <summary>
+        /// This method takes a group of strings, and returns a list of lists of the strings grouped by their first and last letter.
+        /// </summary>
+        /// <param name="strs">strs is the given array of strings.</param>
+        ///  <returns>This method will return the list of lists of strings that are grouped correctly.</returns>
         List<List<string>> GroupStrings(string[] strs);
     }
 }
--- LogicProblems/LogicProblem.cs
+++ LogicProblems/LogicProblem.cs
@@ -4,9 +4,18 @@ using System.Linq;
 
 namespace LogicProblems
 {
+    /// <summary>
+    /// This public class is the class of the file ILogicProblem.
+    /// </summary>
     public class LogicProblem : ILogicProblem
     {
         //TODO: Implement all requirements as defined in the project description.
+        /// <summary>
+        /// This method takes an array of integers, named "scores", and returns a double using the "Average" operation.
+        /// </summary>
+        /// <param name="scores">scores is the array of integers.</param>
+        /// <returns>Method will return the total Average of the array into a double.</returns>
+        /// <exception cref="NotImplementedException">This exception will throw if one or more integers in the array are negative integers.</exception>
         public double Average(int[] scores)
         {
             int i = 0;
@@ -29,6 +38,12 @@ namespace LogicProblems
             return total;
         }
 
+        /// <summary>
+        /// This method takes an integer, named "rungs", and returns another integer displaying the number of times you can reach the given integer.
+        /// </summary>
+        /// <param name="rungs">rungs is the integer given to determine its paths.</param>
+        /// <returns>econdNumber is returned</returns>
+        /// <exception cref="NotImplementedException">This exception will display if the given integer is a negative number.</exception>
         public decimal DistinctLadderPaths(int rungs)
         {
             decimal firstNumber = 1;
@@ -51,6 +66,12 @@ namespace LogicProblems
         }
 
 
+        /// <summary>
+        /// This method takes a group of strings, and returns a list of lists of the strings grouped by their first and last letter.
+        /// </summary>
+        /// <param name="strs">strs is the given array of strings.</param>
+        /// <returns>This method will return the list of lists of strings that are grouped correctly.</returns>
+        /// <exception cref="NotImplementedException">This exception will throw if any of the strings in the given array are empty.</exception>
         public List<List<string>> GroupStrings(string[] strs)
         {
             //make a dictionary
@@ -84,6 +105,12 @@ namespace LogicProblems
             return myList;
         }
 
+        /// <summary>
+        /// This method takes a given string, named "text", and returns the last word's length as an integer.
+        /// </summary>
+        /// <param name="text">text is the given string, such as "Hello World".</param>
+        /// <returns>This method will return lastLength, which is the integer of the last word's length. Ex: "Hello World" -> "World" -> 5.</returns>
+        /// <exception cref="NotImplementedException">This exception will display if the given string is empty. Whitespace is allowed.</exception>
         public int LastWordLength(string text)
         {
             List<string> newText = text.Trim().Split(' ').ToList();
